1b53280cad0c8e243f5a45bf396e11ad69854eb0
Dr.-Ing. Akshay D. Sonawane <146961189+akshayson22@users.noreply.github.com>
2025-09-01 19:44:59 +0200

    Delete ethylene-avocado-webapp directory

--- ethylene-avocado-webapp/static/js/script.js
+++
@@ -1,9 +0,0 @@
-// subtle UI niceties
-document.addEventListener("DOMContentLoaded", () => {
-  const btns = document.querySelectorAll(".btn");
-  btns.forEach(b => {
-    b.addEventListener("mousedown", () => b.style.transform = "translateY(1px) scale(0.995)");
-    b.addEventListener("mouseup", () => b.style.transform = "");
-    b.addEventListener("mouseleave", () => b.style.transform = "");
-  });
-});
